d3588e929f764c78bb38849ecad8b7b9d12c0953
AsgarMadadli <asgarmm@code.edu.az>
2023-06-25 06:40:01 -0700

    task completed

---
+++ personObject/personObject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public Person(string name, string surname, int age, decimal height, int weight)
+        {
+            Name = name;
+            SurName = surname;
+            Age = age;
+            Height = height;
+            Weight = weight;
+        }
+
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        public decimal Height { get; set; }
+        public int Weight { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name{Name}, Surname {SurName}, Age: {Age}, Height: {Height}, Weight: {Weight}";
+        }
+    }
+
+
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person user1 = new Person("Asger", "Mdadli", 15, 1.70m, 56);
+            Console.WriteLine(user1.ToString());
+
+
+
+
+        }
+    }
+}
